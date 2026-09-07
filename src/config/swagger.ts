c646ae3f2f802756cc6a9aabbe6194e272e38ca6
ydj-13 <de2013@naver.com>
2026-09-07 14:01:13 +0900

    feat: #11-스웨거코드 추가

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -23,9 +23,7 @@ const options: swaggerJSDoc.Options = {
     },
   },
   // 개발은 src의 .ts, 프로덕션은 빌드된 dist의 .js를 읽음
-  apis: isProd
-    ? ['./dist/routes/*.js', './dist/swaggertest/*.js']
-    : ['./src/routes/*.ts', './src/swaggertest/*.ts'],
+  apis: isProd ? ['./dist/docs/*.js'] : ['./src/docs/*.ts'],
 };
 
 export const swaggerSpec = swaggerJSDoc(options);
