d1a2fd0bc9ac8e0747a83cf0dfa6fdacccc0b2f6
sgwag6799 <sgwag6799@gmail.com>
2026-09-07 09:52:47 +0900

    fix: validation 파일 원상복기

--- src/middlewares/validation.ts
+++ src/middlewares/validation.ts
@@ -4,20 +4,14 @@ import { ZodType } from 'zod';
 type Source = 'body' | 'query' | 'params';
 
 export const validate = <T>(schema: ZodType<T>, source: Source = 'body') => {
-  return (req: Request, _res: Response, next: NextFunction) => {
+  return (req: Request, res: Response, next: NextFunction) => {
     const result = schema.safeParse(req[source]);
 
     if (!result.success) {
       return next(result.error); // ZodError → errorHandler로 전달
     }
 
-    const parsed = result.data;
-    req.validatedData = {
-      ...(typeof req.validatedData === 'object' && req.validatedData !== null
-        ? req.validatedData
-        : {}),
-      ...(typeof parsed === 'object' && parsed !== null ? parsed : {}),
-    };
+    req.validatedData = result.data;
     next();
   };
 };
